c72e21baf7edc7906eddce7846000db945769f28
Гизетдинов Искандер <gizetiskander@gmail.com>
2021-05-16 13:12:11 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Figure3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelToolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelTools = new System.Windows.Forms.Panel();
+            this.buttonAddEllipse = new System.Windows.Forms.Button();
+            this.buttonAddLine = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.panelTools.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem1,
+            this.toolStripSeparator1,
+            this.saveToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(38, 22);
+            this.toolStripDropDownButton1.Text = "File";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.panelToolsToolStripMenuItem});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(101, 22);
+            this.toolStripMenuItem2.Text = "Tools";
+            // 
+            // panelToolsToolStripMenuItem
+            // 
+            this.panelToolsToolStripMenuItem.Name = "panelToolsToolStripMenuItem";
+            this.panelToolsToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.panelToolsToolStripMenuItem.Text = "Panel Tools";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(101, 22);
+            this.toolStripMenuItem1.Text = "Save";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(98, 6);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.saveToolStripMenuItem.Text = "Exit";
+            // 
+            // panelTools
+            // 
+            this.panelTools.BackColor = System.Drawing.Color.Silver;
+            this.panelTools.Controls.Add(this.button1);
+            this.panelTools.Controls.Add(this.buttonAddEllipse);
+            this.panelTools.Controls.Add(this.buttonAddLine);
+            this.panelTools.Location = new System.Drawing.Point(684, 28);
+            this.panelTools.Name = "panelTools";
+            this.panelTools.Size = new System.Drawing.Size(116, 367);
+            this.panelTools.TabIndex = 5;
+            // 
+            // buttonAddEllipse
+            // 
+            this.buttonAddEllipse.Location = new System.Drawing.Point(21, 26);
+            this.buttonAddEllipse.Name = "buttonAddEllipse";
+            this.buttonAddEllipse.Size = new System.Drawing.Size(69, 50);
+            this.buttonAddEllipse.TabIndex = 1;
+            this.buttonAddEllipse.Text = "Добавить эллипс";
+            this.buttonAddEllipse.UseVisualStyleBackColor = true;
+            // 
+            // buttonAddLine
+            // 
+            this.buttonAddLine.Location = new System.Drawing.Point(21, 82);
+            this.buttonAddLine.Name = "buttonAddLine";
+            this.buttonAddLine.Size = new System.Drawing.Size(69, 53);
+            this.buttonAddLine.TabIndex = 0;
+            this.buttonAddLine.Text = "Добавить линию";
+            this.buttonAddLine.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить квадрат";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.panelTools);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panelTools.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem panelToolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Panel panelTools;
+        private System.Windows.Forms.Button buttonAddEllipse;
+        private System.Windows.Forms.Button buttonAddLine;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
