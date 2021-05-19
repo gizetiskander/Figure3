da16f3cbc0eed724c8048689666838991303fe3d
Гизетдинов Искандер <gizetiskander@gmail.com>
2021-05-19 15:07:49 +0300

    i try to add save and change panel to picturebox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,12 +34,13 @@
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.panelTools = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.toolStrip1.SuspendLayout();
             this.panelTools.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // toolStrip1
@@ -69,6 +70,8 @@
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
             this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
             this.toolStripMenuItem1.Text = "Save";
+            this.toolStripMenuItem1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
             // 
             // toolStripSeparator1
             // 
@@ -86,24 +89,6 @@
             this.panelTools.Size = new System.Drawing.Size(116, 367);
             this.panelTools.TabIndex = 5;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(23, 25);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(69, 53);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Нарисовать ";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(23, 84);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(69, 53);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Очистить";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
@@ -117,13 +102,34 @@
             this.comboBox1.Size = new System.Drawing.Size(92, 21);
             this.comboBox1.TabIndex = 4;
             // 
-            // panel1
+            // button2
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.Control;
-            this.panel1.Location = new System.Drawing.Point(73, 44);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(531, 351);
-            this.panel1.TabIndex = 7;
+            this.button2.Location = new System.Drawing.Point(23, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 53);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Нарисовать ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(118, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(451, 335);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
@@ -131,14 +137,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GrayText;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.panelTools);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.button1_Click);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.panelTools.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -154,7 +162,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,19 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
 
 namespace Figure3
 {
     public partial class Form1 : Form
     {
+        private Bitmap bmp;
         public Form1()
         {
             InitializeComponent();
+            bmp = new Bitmap(700, 900);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var graphics = panel1.CreateGraphics();
+            var graphics = pictureBox1.CreateGraphics();
             Pen pn = new Pen(Color.Black, 5);
             string combo = comboBox1.Text;
             if (combo == "Отрезок")
@@ -53,8 +56,26 @@ namespace Figure3
         }
         private void button2_Click(object sender, EventArgs e)
         {
-            var graphics = panel1.CreateGraphics();
+            var graphics = pictureBox1.CreateGraphics();
             graphics.Clear(Color.White);
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+                SaveFileDialog savedialog = new SaveFileDialog();
+                savedialog.Title = "Сохранить картинку как...";
+                savedialog.Filter = "Image Files(*.BMP)|*.BMP|Image Files(*.JPG)|*.JPG|All files (*.*)|*.*";
+                if (pictureBox1.Image != null)
+                {
+                    try
+                    {
+                        bmp.Save(savedialog.FileName, ImageFormat.Jpeg);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
         }
     }
 }
